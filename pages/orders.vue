65baf0e8b37d6df94e13a837de92db1d47015126
Dr. Lau Cher Han <laucherhan@gmail.com>
2022-04-21 01:04:54 +0800

    updated UI

--- pages/orders.vue
+++ pages/orders.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="bg-[url('/menu-page.jpg')] px-20">
-    <div id="menu" class="grid grid-cols-2 py-5">
-      <div id="menu-left">
-        <img src="/logo.png" class="w-[150px]" alt="" />
+  <div>
+    <div class="bg-[url('/menu-page.jpg')] px-20">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="/logo.png" class="w-[150px]" alt="" />
+        </div>
+
+        <div id="menu-right">
+          <ul class="grid grid-cols-5 text-lg font-oswald text-white uppercase pt-5">
+            <li>About Us</li>
+            <NuxtLink to="/menu">Our Menu</NuxtLink>
+            <li>Locations</li>
+            <li>Contacts</li>
+            <NuxtLink to="/cart">Cart</NuxtLink>
+          </ul>
+        </div>
       </div>
 
-      <div id="menu-right">
-        <ul class="grid grid-cols-5 text-lg font-oswald text-white uppercase pt-5">
-          <li>About Us</li>
-          <NuxtLink to="/menu">Our Menu</NuxtLink>
-          <li>Locations</li>
-          <li>Contacts</li>
-          <NuxtLink to="/cart">Cart</NuxtLink>
-        </ul>
-      </div>
+      <h1 class="text-white font-oswald uppercase text-6xl text-center pt-16 pb-28">
+        Orders
+      </h1>
     </div>
-
-    <h1 class="text-white font-oswald uppercase text-6xl text-center pt-16 pb-28">
-      Orders
-    </h1>
 
     <table
       v-for="order in orders"
